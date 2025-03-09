1918ed8923d98cad00976e4cee595904e3be75d6
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-03-09 12:39:31 -0700

    Refactor link filter to use a taxonomy rather than post format.

--- lib/functions.php
+++ lib/functions.php
@@ -4,19 +4,6 @@
  */
 
 /**
- * Enable theme support for post formats
- */
-
-add_theme_support( 'post-formats', array( 'link' ) );
-
-// Remove post-format support for posts
-add_action( 'init', 'ucscgiving_remove_post_format_support' );
-
-function ucscgiving_remove_post_format_support() {
-	remove_post_type_support( 'post', 'post-formats' );
-}
-
-/**
  * Register custom ACF Text Meta Fields for Block binding
  *
  * Fields are defined in the ACF UI in the WP Dashboard
@@ -87,53 +74,25 @@ add_filter('post_type_link', 'ucscgiving_link_filter', 10, 2);
 function ucscgiving_link_filter($post_link, $post) {
 		$baseurl = get_field('base_url', 'option');
 		$designation = get_post_meta( get_the_ID(), 'designation', true );
+		$termID = get_field('priority-fund');
+		$term = get_term($termID);
 		$fundurl = '';
 		if ( ( 'fund' === $post->post_type ) ) {
 			
-			if (has_post_format('link', $post)){
-				if ( !empty( $baseurl ) && !empty( $designation ) ) { 
-					$fundurl = esc_attr($baseurl . $designation);
-				} else if ( !empty( $baseurl ) ) {
-					$fundurl = esc_attr($baseurl);
-				} else {
-					$fundurl = '';
+			if( ! is_wp_error( $term ) ) {
+				$term_name = $term->name;
+				if ($term_name === 'Link') {
+					if ( !empty( $baseurl ) && !empty( $designation ) ) { 
+						$fundurl = esc_attr($baseurl . $designation);
+					} else if ( !empty( $baseurl ) ) {
+						$fundurl = esc_attr($baseurl);
+					} else {
+						$fundurl = '';
+					}
+					return $fundurl;
 				}
-				return $fundurl;
 			}
 		}
 		return $post_link;
 }
 
-
-/**
- * Customize Admin Columns for Fund Post Type
- */
-$post_type = 'fund';
-// Add attribute support for the post type
-add_post_type_support( $post_type, 'page-attributes' );
-
-// Register the columns
-add_filter( 'manage_'.$post_type.'_posts_columns', 'ucscgiving_fund_columns' );
-
-function ucscgiving_fund_columns( $columns ) {
-	$columns['format'] = __('Format', 'ucsccgiving');
-	return $columns;
-}
-
-// Populate the columns
-add_action( 'manage_'. $post_type .'_posts_custom_column', 'ucscgiving_fund_columns_data', 10, 2 );
-
-function ucscgiving_fund_columns_data( $column, $post_id ) {
-	switch ( $column ) {
-		case 'format' :
-			echo get_post_format() ? : 'Priority';
-			break;
-	}
-}
-
-// Make the columns sortable
-add_filter( 'manage_edit-'.$post_type.'_sortable_columns', 'ucscgiving_fund_sortable_columns' );
-function ucscgiving_fund_sortable_columns( $columns ) {
-	$columns['format'] = __('Format', 'ucsccgiving');
-	return $columns;
-}
