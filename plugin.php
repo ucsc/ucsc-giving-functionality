4e68ec5cb584bc87f9def0343e154f0457c46d15
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-03-20 10:12:57 -0700

    Add and enqueue `admin-settings.css`

--- plugin.php
+++ plugin.php
@@ -28,6 +28,35 @@ if ( file_exists( UCSC_GIVING_PLUGIN_DIR . 'lib/functions.php' ) ) {
 if ( file_exists( UCSC_GIVING_PLUGIN_DIR . '/lib/settings.php' ) ) {
 	include_once UCSC_GIVING_PLUGIN_DIR . '/lib/settings.php';
 }
+// Enqueue admin settings styles.
+if ( ! function_exists( 'ucscgiving_enqueue_admin_styles' ) ) {
+	/**
+	* Enqueue admin settings styles
+	*
+	* No styles are enqueued for raw HTML in setting panel.
+	* In order to output HTML in the settings panel we need some basic styles.
+	*
+	* @since 0.5.0
+	 *
+	 * @author UCSC
+	 *
+	 * @link https://developer.wordpress.org/reference/hooks/admin_enqueue_scripts/#Example:_Load_CSS_File_from_a_plugin_on_specific_Admin_Page
+	 */
+
+	function ucscgiving_enqueue_admin_styles($hook): void {
+		$settings_css   = plugin_dir_url( __FILE__ ) . 'lib/css/admin-settings.css';
+		$current_screen = get_current_screen();
+		// Check if it's "?page=ucsc-giving-functionality-settings." If not, just empty return. 
+		if ( strpos( $current_screen->base, 'ucsc-giving-functionality-settings' ) === false ) {
+			return;
+		}
+
+		// Load css.
+		wp_register_style( 'ucscgiving-cf-admin-settings', $settings_css, );
+		wp_enqueue_style( 'ucscgiving-cf-admin-settings' );
+	}
+}
+add_action( 'admin_enqueue_scripts', 'ucscgiving_enqueue_admin_styles' );
 
 // Set plugin directory for syncing ACF JSON files.
 add_filter( 'acf/settings/save_json', 'ucscgiving_acf_json_save_point' );
@@ -43,3 +72,4 @@ function ucscgiving_acf_json_load_point( $paths ) {
 	$paths[] = UCSC_GIVING_PLUGIN_DIR . 'acf-json';
 	return $paths;
 }
+
