70e218b7074ff8c20cc6f3c972dacce15f4a0f38
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-03-24 09:21:17 -0700

    add proper class to funds query

--- lib/templates/parts/post-query-funds.php
+++ lib/templates/parts/post-query-funds.php
@@ -1,6 +1,6 @@
 
 <!-- wp:query {"queryId":0,"query":{"perPage":9,"pages":0,"offset":0,"postType":"post","order":"asc","orderBy":"title","author":"","search":"","exclude":[],"sticky":"","inherit":true},"tagName":"main","layout":{"type":"constrained"}} -->
-<main class="wp-block-query"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
+<main class="wp-block-query ucsc__post-query-loop"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
 <!-- wp:group {"className":"ucsc__card ucsc__card\u002d\u002dquery-loop","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"var:preset|spacing|20"},"border":{"radius":"5px"}},"backgroundColor":"lightest-gray","layout":{"type":"default"}} -->
 <div class="wp-block-group ucsc__card ucsc__card--query-loop has-lightest-gray-background-color has-background" style="border-radius:5px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"16/9","width":"","height":"","sizeSlug":"sixteen-nine","className":"ucsc-query-loop__image","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} /-->
 
