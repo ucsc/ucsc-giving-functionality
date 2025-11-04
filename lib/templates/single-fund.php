960fa8fface29e4034da5845fb305a050e4f8321
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-11-04 15:39:07 -0800

    Back-up Funds templates Fixes #16

--- lib/templates/single-fund.php
+++ lib/templates/single-fund.php
@@ -1,25 +1,31 @@
-<!-- wp:template-part {"slug":"header","theme":"ucsc-2022","tagName":"header","className":"header-region"} /-->
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"header-region"} /-->
 
-<!-- wp:template-part {"slug":"breadcrumbs","theme":"ucsc-2022","tagName":"section","className":"breadcrumbs-region"} /-->
+<!-- wp:template-part {"slug":"breadcrumbs","tagName":"section","className":"breadcrumbs-region"} /-->
 
-<!-- wp:group {"tagName":"main","className":"content-region","layout":{"type":"default"}} -->
-<main class="wp-block-group content-region"><!-- wp:group {"layout":{"type":"constrained","contentSize":"44rem","wideSize":"80rem"}} -->
-<div class="wp-block-group"><!-- wp:group {"metadata":{"categories":["banner"],"patternName":"ucsc-2022/page-banner-title","name":"Banner with page title"},"align":"full","className":"ucsc-page-header","fontSize":"base","layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull ucsc-page-header has-base-font-size"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":70,"isUserOverlayColor":true,"focalPoint":{"x":0.91,"y":1},"gradient":"ucsc-headline-gradient","contentPosition":"center center","className":"ucsc-page-header__content","style":{"spacing":{"padding":{"top":"var:preset|spacing|50"}}}} -->
-<div class="wp-block-cover ucsc-page-header__content" style="padding-top:var(--wp--preset--spacing--50)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-70 has-background-dim has-background-gradient has-ucsc-headline-gradient-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:post-title {"level":1,"className":"ucsc-page-header__title primary-post-title","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"backgroundColor":"ucsc-primary-yellow","textColor":"ucsc-primary-blue","fontSize":"seven"} /--></div>
+<!-- wp:group {"className":"fund-banner","style":{"spacing":{"padding":{"top":"0px","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group fund-banner" style="padding-top:0px;padding-bottom:0"><!-- wp:group {"metadata":{"categories":["banner"],"patternName":"ucsc-2022/page-banner-title","name":"Banner with page title"},"align":"full","className":"ucsc-page-header","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"fontSize":"base","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull ucsc-page-header has-base-font-size" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":70,"isUserOverlayColor":true,"focalPoint":{"x":0.5,"y":0},"minHeight":450,"customGradient":"linear-gradient(90deg,rgb(0,60,99) 38%,rgba(0,0,0,0) 84%)","contentPosition":"top center","align":"full","style":{"spacing":{"padding":{"top":"2.5rem","left":"0px","right":"0px","bottom":"0px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-top-center" style="padding-top:2.5rem;padding-right:0px;padding-bottom:0px;padding-left:0px;min-height:450px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-70 has-background-dim has-background-gradient" style="background:linear-gradient(90deg,rgb(0,60,99) 38%,rgba(0,0,0,0) 84%)"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"right":"1rem","left":"1rem"}}},"layout":{"type":"constrained","contentSize":"1280px"}} -->
+<div class="wp-block-group" style="padding-right:1rem;padding-left:1rem"><!-- wp:group {"layout":{"type":"constrained","contentSize":"600px","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:buttons {"style":{"spacing":{"margin":{"bottom":"1rem"}}}} -->
+<div class="wp-block-buttons" style="margin-bottom:1rem"><!-- wp:button {"textColor":"ucsc-primary-blue","metadata":{"bindings":{"text":{"source":"acf/field","args":{"key":"button-text"}},"url":{"source":"ucscgiving/fund-url"}}},"className":"is-style-ucsc-gold","style":{"elements":{"link":{"color":{"text":"var:preset|color|ucsc-primary-blue"}}}}} -->
+<div class="wp-block-button is-style-ucsc-gold"><a class="wp-block-button__link has-ucsc-primary-blue-color has-text-color has-link-color wp-element-button"></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:post-title {"level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"lineHeight":"1.1","fontStyle":"normal","fontWeight":"700"}},"textColor":"white","fontSize":"eight","fontFamily":"roboto"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"74px"} -->
+<div style="height:74px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:post-content /-->
-
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"metadata":{"bindings":{"text":{"source":"acf/field","args":{"key":"button-text"}},"url":{"source":"ucscgiving/fund-url"}}},"className":"is-style-ucsc-blue"} -->
-<div class="wp-block-button is-style-ucsc-blue"><a class="wp-block-button__link wp-element-button"></a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group --></main>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--20)"><!-- wp:post-content {"layout":{"type":"constrained","contentSize":"846px","wideSize":"1280px"}} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:template-part {"slug":"footer","theme":"ucsc-2022","tagName":"footer","className":"footer-region"} /-->
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"footer-region"} /-->
