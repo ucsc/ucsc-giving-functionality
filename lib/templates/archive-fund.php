b26cfdb7b4bb237e04c63f096cbcb3e62d9214a3
Jason Chafin <jchafin@ucsc.edu>
2025-11-06 13:57:48 -0800

    Back-up Funds templates (#17)
    Fixes #16

--- lib/templates/archive-fund.php
+++ lib/templates/archive-fund.php
@@ -1,13 +1,75 @@
-<!-- wp:template-part {"slug":"header","theme":"ucsc-2022","tagName":"header","className":"header-region"} /-->
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"header-region"} /-->
 
-<!-- wp:template-part {"slug":"breadcrumbs","theme":"ucsc-2022","tagName":"section","className":"breadcrumbs-region"} /-->
+<!-- wp:template-part {"slug":"breadcrumbs","tagName":"section","className":"breadcrumbs-region"} /-->
 
-<!-- wp:group {"tagName":"main","className":"content-region","style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}}},"layout":{"inherit":true,"type":"constrained"}} -->
-<main class="wp-block-group content-region" style="margin-top:var(--wp--preset--spacing--50)">
-<!-- wp:query-title {"type":"archive","textAlign":"center","showPrefix":false} /-->
-<?php require 'parts/funds-search.php'; ?>
-<?php require 'parts/post-query-funds.php'; ?>
-</main>
+<!-- wp:cover {"url":"https://giving.wordpress.ucsc.edu/files/2025/07/Kasey-Morales-UCSC-004.jpg","id":6256,"dimRatio":0,"isUserOverlayColor":true,"focalPoint":{"x":0.35999999999999999,"y":1},"customGradient":"linear-gradient(90deg,rgba(0,60,100,0.5) 15%,rgba(0,0,0,0) 54%)","contentPosition":"top center","sizeSlug":"full","align":"full","className":"dcs_cover-innerContainer-maxWidthOverride dcs_posFix_003","style":{"spacing":{"padding":{"top":"2.5rem","left":"0px","right":"0px","bottom":"0px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-top-center dcs_cover-innerContainer-maxWidthOverride dcs_posFix_003" style="padding-top:2.5rem;padding-right:0px;padding-bottom:0px;padding-left:0px"><img class="wp-block-cover__image-background wp-image-6256 size-full" alt="" src="https://giving.wordpress.ucsc.edu/files/2025/07/Kasey-Morales-UCSC-004.jpg" style="object-position:36% 100%" data-object-fit="cover" data-object-position="36% 100%"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim has-background-gradient" style="background:linear-gradient(90deg,rgba(0,60,100,0.5) 15%,rgba(0,0,0,0) 54%)"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","contentSize":"1280px"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0rem","left":"0rem"}}},"layout":{"type":"constrained","contentSize":"50rem","justifyContent":"left","wideSize":"40rem"}} -->
+<div class="wp-block-group" style="padding-right:0rem;padding-left:0rem"><!-- wp:query-title {"type":"archive","textAlign":"left","showPrefix":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"lineHeight":"1.1","fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"0"}},"color":{"gradient":"linear-gradient(90deg,rgb(0,60,108) 34%,rgba(20,104,168,0) 68%)"}},"textColor":"white","fontSize":"eight","fontFamily":"roboto"} /-->
+
+<!-- wp:spacer {"height":"14.69rem"} -->
+<div style="height:14.69rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:template-part {"slug":"footer","theme":"ucsc-2022","tagName":"footer","className":"footer-region"} /-->
+<!-- wp:cover {"url":"https://giving.wordpress.ucsc.edu/files/2025/08/BG_4c.png","id":6294,"dimRatio":0,"customOverlayColor":"#08497d","isUserOverlayColor":false,"contentPosition":"top center","sizeSlug":"full","style":{"spacing":{"padding":{"top":"0rem","left":"0px","right":"0px"},"margin":{"bottom":"0px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-top-center" style="margin-bottom:0px;padding-top:0rem;padding-right:0px;padding-left:0px"><img class="wp-block-cover__image-background wp-image-6294 size-full" alt="" src="https://giving.wordpress.ucsc.edu/files/2025/08/BG_4c.png" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#08497d"></span><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"14.63rem","className":"dcs_mobileHide"} -->
+<div style="height:14.63rem" aria-hidden="true" class="wp-block-spacer dcs_mobileHide"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained","contentSize":"1060px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"spacing":{"padding":{"bottom":"8px"}}},"fontSize":"four"} -->
+<h2 class="wp-block-heading has-four-font-size" style="padding-bottom:8px">Search All Funds</h2>
+<!-- /wp:heading -->
+
+<!-- wp:search {"label":"Search Funds","showLabel":false,"placeholder":"Search all funds","width":720,"widthUnit":"px","buttonText":"Search Funds","buttonUseIcon":true,"query":{"post_type":"fund"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|ucsc-primary-blue"}}}},"backgroundColor":"ucsc-university-yellow","textColor":"ucsc-primary-blue"} /-->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"16px"}}}} -->
+<p style="margin-top:16px">Enter a word or words in the search box above, or use the drop down lists below to make a selection.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"1rem"} -->
+<div style="height:1rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"level":3,"fontSize":"base"} -->
+<h3 class="wp-block-heading has-base-font-size">Search by Area or Division</h3>
+<!-- /wp:heading -->
+
+<!-- wp:categories {"taxonomy":"area","displayAsDropdown":true,"showLabel":false} /-->
+
+<!-- wp:spacer {"height":"1rem"} -->
+<div style="height:1rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:query {"queryId":7,"query":{"perPage":12,"pages":0,"offset":0,"postType":"fund","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"parents":[]},"enhancedPagination":true,"metadata":{"categories":["posts"],"patternName":"core/query-grid-posts","name":"Grid"}} -->
+<div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"layout":{"inherit":false}} -->
+<div class="wp-block-group" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:post-featured-image {"sizeSlug":"large"} /-->
+
+<!-- wp:post-title {"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|ucsc-primary-yellow"}}}},"spacing":{"padding":{"top":"var:preset|spacing|20"}}}} /-->
+
+<!-- wp:post-excerpt /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
+<!-- wp:query-pagination -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"7rem"} -->
+<div style="height:7rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:cover --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"footer-region"} /-->
